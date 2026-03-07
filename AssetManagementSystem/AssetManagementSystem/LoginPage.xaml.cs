cd76ee8837b691b28e34bd26499e4243d90addcc
Pure <nikitanikitin721@gmail.com>
2026-03-07 16:44:56 +0600

    wpf проект

---
+++ AssetManagementSystem/AssetManagementSystem/LoginPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace AssetManagementSystem
+{
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Функция входа пока в разработке");
+        }
+
+        private void OpenRegistration(object sender, RoutedEventArgs e)
+        {
+            RegistrationPage reg = new RegistrationPage();
+            reg.Show();
+            this.Close();
+        }
+    }
+}
---
+++ AssetManagementSystem/AssetManagementSystem/RegistrationPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace AssetManagementSystem
+{
+    public partial class RegistrationPage : Window
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            if (PasswordBox.Password != ConfirmPasswordBox.Password)
+            {
+                MessageBox.Show("Пароли не совпадают!");
+                return;
+            }
+
+            MessageBox.Show("Регистрация в разработке");
+        }
+    }
+}
